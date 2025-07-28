ad748b5d34de0f3a53a46d59c0e43ff7d9f8eea2
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-07-28 16:42:22 +0330

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,109 @@
+# emroz avalin jalase ma bood
+
+'''
+salam
+omidvaram hale shoma khob bashad
+chetorin shoma
+'''
+
+
+# print("hello world")
+
+# VARIABLE
+# name = 'amirhossein'
+
+# print(name)
+# print("name")
+
+# score = 20
+
+# username1 = "amirhossein"
+# user_name1 = "amirhossein"
+# username = "amir hossein"
+# user!name1 = "amirhossein"
+
+
+
+# lowerCamelCase
+# userName="amir"
+# schoolName="alghadir"
+# studentMathScore=20
+
+
+# username = input("please enter username: ")
+# print(username)
+
+# name = "amir"
+# print(name)
+
+# print("amirhossein",end="  ")
+# print("hajitabar")
+# print("from babol")
+
+# SLICING
+# name = "amir hajitabar"
+# print(name[4])
+# print(name[-1])
+# print(name[1:9])
+# print(name[:9])
+# print(name[5:])
+# print(name[1:9:2])
+# print(name[::-1])
+
+# name="amir"
+# print(name.upper())
+
+# lastName="HAJITABAR"
+# print(lastName.lower())
+
+# message = "i`m from iran"
+# print(message.title())
+
+# name = "amir reza"
+# print(name.replace("r","h"))
+
+# products = "apple samsung xiaomi"
+# splitedProducts = products.split(" ")
+# print(splitedProducts)
+
+# amir  
+#  amir
+
+# username = "   amir  "
+# print(username.lstrip())
+# print(username.rstrip())
+# print(username.strip())
+
+# firstName = "amir"
+# lastName = "hajitabar"
+# fullName = firstName+lastName
+# print(fullName)
+
+# print(f"welcome mr/mrs {firstName} {lastName}")
+# print("welcome mr/mrs {} {}".format(firstName,lastName))
+# print("welcome mr/mrs " + firstName + lastName)
+# print("welcome mr/mrs " , firstName , lastName)
+
+# print("amirhossein\nhajitabar\tprogramming teacher")
+
+'''
+int
+float
+'''
+
+# score = 20
+
+# a=5
+# b=2
+# print(a-b)
+# print(a+b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# print("14"*5)
+
+score = int(input("please enter score: "))
+print(score*5)
