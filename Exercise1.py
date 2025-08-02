a70ad1f582326de1fa73a128b411b11d6dc777b8
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-08-02 14:54:26 +0330

    Create Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,59 @@
+# E1
+# date = "2025-07-23"
+# date_updated = date.replace("-","/")
+# print(date_updated)
+
+# E2
+# a=int(input("enter a:"))
+# b=int(input("enter b:"))
+# c=int(input("enter c:"))
+# x=int(input("enter x:"))
+
+# y=a*x**2+b*x+c
+# print("y:",y)
+
+# print(a*x**2+b*x+c)
+
+# a=float(input("please enter a: "))
+# b=float(input("please enter b: "))
+# c=float(input("pleas enter c: "))
+# x=float(input("pleas enter x: "))
+# valuey= a*x**2 + b*x + c
+# print(valuey)
+
+
+# E3
+# name = input("please enter your name: ")
+# favorite_programming_language1 = input("please enter your favorite programming language: ")
+# favorete_programming_language2 = input("please enter your favorite programming language: ")
+# favorite_programming_language3 = input("please enter your favorite programming language: ")
+
+# '''
+# my name is AMIR:
+#     1-js
+#     2-php
+#     3-python
+# '''
+
+# print(f"my name is {name.upper()}:\n\t1-{favorite_programming_language1}\n\t2-{favorete_programming_language2}\n\t3-{favorite_programming_language3}")
+
+# E4
+# n1 = int(input())
+# n2 = int(input())
+# n3 = int(input())
+# n4 = int(input())
+# n5 = int(input())
+# print ((n1+n2+n3+n4+n5)/5)
+
+# E5
+# c = int(input("Enter temperature: "))
+# f = (c*1.8)+32
+# print(f)
+
+# E6
+# num1=int(input("please enter num1="))
+# num2=int(input("please enter num2="))
+# temp=num1
+# num1=num2
+# num2=temp
+# print(num1,num2)
