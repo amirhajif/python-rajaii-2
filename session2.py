6a12c590c0fd25721177df7635ffa70f6f36927b
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-08-04 15:20:36 +0330

    Create session2.py

---
+++ session2.py
@@ -0,0 +1,123 @@
+# money = 1_000_000
+# print(money)
+
+# multiple assignment   
+# num1,num2,num3=int(input("num 1: ")),int(input("num 2: ")),int(input("num 3: "))
+
+
+'''
+Collection:
+1-List+ []
+2-Tuple
+3-Dictionary+
+4-set
+'''
+# scores = [20,15,14,9,3,8,14]
+# print(type(scores))
+# print(scores[0])
+# print(scores[1:4])
+
+# users = ["amir","rezza","ahmad"]
+
+# # Update Item
+# # users[1]="reza"
+# # print(users)
+
+# # Append Item
+# users.append("mohammad")
+# print(users)
+
+# # Insert
+# users.insert(1,"danial")
+# print(users)
+
+# # del
+# del users[3]
+# print(users)
+
+# # pop
+# popedItem=users.pop()
+# print(users)
+# print(popedItem)
+
+# # pop at index
+# index1DeletedItem=users.pop(1)
+# print(users)
+# print(index1DeletedItem)
+
+# # remove
+# users.remove("amir")
+# print(users)
+
+'''
+users= ["amir","reza","ahmad","amir"]
+users.remove("amir")
+'''
+
+# scores = [20,13,14,5,9,12]
+# scores.sort()
+# print(scores)
+# scores.sort(reverse=True)
+# print(scores)
+
+# users = ["amir","rezza","ahmad"]
+# # # users.sort()
+# # # print(users)
+# newList = sorted(users)
+# print(newList)
+# print(users)
+
+# scores = [20,13,14,5,9,12]
+# # scores.reverse()
+# # print(scores)
+# reversedList = list(reversed(scores))
+# print(reversedList)
+
+# len:length
+# username = "amirhossein"
+# print(len(username))
+# scores = [20,13,14,5,9,12]
+# print(len(scores))
+
+# scores = [20,13,14,5,9,12]
+# print(max(scores))
+# print(min(scores))
+# print(sum(scores))
+
+# print(sum(scores)/len(scores))
+
+# a=10
+# b=a
+# a=20
+# print(a,b)
+
+# students=["amir","reza","ahmad"]
+# newStudents = students
+# newStudents.append("mamad")
+# students.append("dost e mamad")
+# print(students)
+# print(newStudents)
+
+# students=["amir","reza","ahmad"]
+# newStudents = students[:]
+# newStudents.append("mamad")
+# students.append("dost e mamad")
+# print(students)
+# print(newStudents)
+
+# score = int(input("please enter score: "))
+# if score>=10:
+#     print("pass")
+# else:
+#     print("fail")
+
+a=int(input("please enter a: "))
+b=int(input("please enter b: "))
+# if a==b:
+#     print("Equal")
+# else:
+#     print("not-equal")
+# if a != b:
+#     print("not-Equal")
+# else:
+#     print("Equal")
