92be54b8b5e38c2396c1c12f1b907af3ad30e986
amirhossein hajitabar <amirhajitabar2@gmail.com>
2025-09-06 15:59:53 +0330

    Create session9.py

---
+++ session9.py
@@ -0,0 +1,64 @@
+# OS
+# import os
+
+# current location
+# print(os.getcwd())
+
+# currentLocation = os.getcwd()
+# print(currentLocation)
+
+
+# change direction
+# os.chdir("folder1/")
+# print(os.getcwd())
+
+# print(os.getcwd())
+
+# os.chdir("folder1/folder4")
+# print(os.getcwd())
+
+# os.chdir("../folder3")
+# print(os.getcwd())
+
+# os.chdir("../../folder2")
+# print(os.getcwd())
+
+
+# print(os.getcwd())
+# os.makedirs("result")
+
+# print(os.getcwd())
+# os.rename("description.txt","info.txt")
+
+# os.remove("info.txt")
+
+
+# productsFile = open("products.txt","r")
+# productsFile = open("products.txt")
+# print(productsFile.read())
+
+# for product in productsFile:
+#     print(product.strip())
+
+# productsList = productsFile.readlines()
+# print(productsList)
+
+# create file , write -> w
+# newFile = open("newFile.txt","w")
+# newFile.write("hello\nhow are you?")
+# newFile.write("\nthank you and you?")
+
+# newFile = open("newFile.txt","a")
+# newFile.write("\nthank you and you?")
+
+# error handling
+# print(5/0)
+
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("we cant divide by zero")
+# else:
+#     print("you see we dont have error")
+# finally:
+#     print("finish course")
